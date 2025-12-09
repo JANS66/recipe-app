bdd29036094a6881b5846b9e47559071dcf8423f
JANS66 <98973687+JANS66@users.noreply.github.com>
2025-12-09 16:11:08 +0200

    feat: add RecipeForm component for creating new recipes

---
+++ src/components/RecipeForm.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react"
+
+export default function RecipeForm({ addRecipe }) {
+    const [title, setTitle] = useState(``)
+    const [ingredients, setIngredients] = useState(``)
+
+    // `handleSubmit` handles the form submission for adding a new recipe.
+    // e.preventDefault() stops the page from reloading on form submit.
+    // It calls `addRecipe` with the current `title` and `ingredients` values to update the recipes state.
+    // After adding, it clears the input fields by resetting `title` and `ingredients` to empty strings.
+    const handleSubmit = e => {
+        e.preventDefault()
+        addRecipe({ title, ingredients })
+        setTitle(``)
+        setIngredients(``)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input
+                placeholder="Recipe title"
+                value={title}
+                onChange ={e => setTitle(e.target.value)}
+            />
+            <textarea
+                placheolder="Ingredients"
+                value={ingredients}
+                onChange={e => setIngredients(e.target.value)}
+            />
+            <button>Add Recipe</button>
+        </form>
+    )
+}
